25035d412e808fda5653d7d8c6c48536eb3c27e1
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2023-10-17 21:12:29 +0600

    Adding rating

--- src/app/modules/review/review.model.ts
+++ src/app/modules/review/review.model.ts
@@ -6,6 +6,7 @@ const reviewSchema = new Schema<IReview>(
     user_id: { type: String, required: true },
     service_id: { type: String, required: true },
     comment: { type: String, required: true },
+    rating: { type: Number, required: true },
   },
   {
     timestamps: true,
--- src/app/modules/review/review.validation.ts
+++ src/app/modules/review/review.validation.ts
@@ -5,6 +5,7 @@ const addReviewZodSchema = z.object({
     user_id: z.string().nonempty({ message: 'User ID is required' }),
     service_id: z.string().nonempty({ message: 'Service ID is required' }),
     comment: z.string().nonempty({ message: 'Comment is required' }),
+    rating: z.number().int().min(1).max(5),
   }),
 });
 
